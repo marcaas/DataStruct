daa5756842a469c96c0c9e2fba29b160561f93cc
marcaas <t13274515361@.gmail.com>
2022-01-03 15:13:38 +0800

    2022/1/3

--- Struct_SqList.cpp
+++ Struct_SqList.cpp
@@ -1,13 +1,13 @@
 # include <stdio.h>
 # include <stdlib.h>
-
+// 静态分配顺序表定义
 #define MaxSize 10
 typedef struct SqList
 {
     int data[MaxSize];
     int Length;
 };
-
+// 插入操作
 bool ListInsert(SqList &L,int i,int e){
     if(i<1||i>L.Length)
         return false;
@@ -19,7 +19,7 @@ bool ListInsert(SqList &L,int i,int e){
     L.Length++;
     return true;
 }
-
+// 删除操作
 bool ListDelete(SqList &L,int i,int &e){
     if(i>L.Length||i<1)
         return false;
@@ -29,7 +29,22 @@ bool ListDelete(SqList &L,int i,int &e){
     L.Length--;
     return true;
 }
-
+// 按值查找操作
+int LocateElem(SqList L,int e){
+    for(int i = 0;i<L.Length;i++)
+        if(L.data[i] == e)
+            return i+1;
+    return 0;
+}
+//按位查找
+int GetElem(SqList L,int i){
+    if(i<1||i>L.Length){
+        printf("输入位置不合法");
+        return 0;
+    }
+    return L.data[i-1];
+}
+//测试
 int main(){
     SqList L;
     L.Length = 0;
@@ -46,6 +61,10 @@ int main(){
     ListDelete(L,2,e);
     for(int i = 0;i<L.Length;i++)
         printf("%d\n",L.data[i]);
-        printf("%d",e);
+    printf("%d\n",e);
+    int ans = LocateElem(L,2);
+    printf("%d\n",ans);
+    ans = GetElem(L,9);
+    printf("%d\n",ans);
     return 0;
 }
